b1f4de649cf9908e69fcce1e892d1e6311b65801
Thae Ei Mon <73652374+Thae-eimon@users.noreply.github.com>
2023-08-06 08:48:39 +0630

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,6 +4,7 @@ import {motion} from 'framer-motion'
 const Contact = () => {
   return (
     <div name="contact" className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4 '>
+
          <motion.div className='text-container'
             initial={{opacity: 0, scale: 0.3}}
             animate={{ y: [200], opacity: 1, scale: 1 }}
@@ -14,7 +15,7 @@ const Contact = () => {
             viewport={{once:true}}
             
             >
-        <form method="POST" action="https://getform.io/f/377bb29d-f1fc-46b5-8820-96de191f7d71" className='flex flex-col max-w-[600px] w-full'>
+        <form method="POST" action="https://getform.io/f/377bb29d-f1fc-46b5-8820-96de191f7d71" className='flex flex-col max-w-[600px] w-full h-full'>
           <div className='md:text-center sm:text-right pb-8'>
             <p className='text-gray-300 text-4xl font-bold inline border-b-4 border-pink-600'>Contact</p>
             <p className='text-gray-300 py-4'>Submit the form below or shoot me an email - thaeeii16@gmail.com</p>
@@ -25,7 +26,8 @@ const Contact = () => {
           <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
         </form>
         </motion.div>
-    </div>
+      </div>
+    
   )
 }
 
